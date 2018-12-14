58e64de4853b28b8da8707fb59ac1039b7a5b90f
bikmay <artyom.ivliev@gmail.com>
2018-12-14 12:19:32 +0400

    create servlet

--- src/IndexServlet.java
+++ src/IndexServlet.java
@@ -1,2 +1,43 @@
-public class IndexServlet {
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class IndexServlet extends HttpServlet {
+
+    public void init(ServletConfig servletConfig) {
+        try {
+            super.init(servletConfig);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.getRequestDispatcher("index.html").forward(req,resp);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            {
+
+    }
+
+    @Override
+    protected void	doPut(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp)  {
+
+    }
+
 }
